5c99fcae11f01659a58778ac801ed007ed0fc402
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2022-05-15 00:07:28 -0600

    Bug Fixes

--- MarketSegmentation/settings.py
+++ MarketSegmentation/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    'market-segmentation-clusterer.herokuapp.com/static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'market-segmentation-clusterer.herokuapp.com/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

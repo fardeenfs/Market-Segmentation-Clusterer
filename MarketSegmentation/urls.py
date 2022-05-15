92b0f57bf27bd6cda8840a39c49524d1034e4589
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2022-05-15 00:38:25 -0600

    Bug Fixes

--- MarketSegmentation/urls.py
+++ MarketSegmentation/urls.py
@@ -17,9 +17,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from MarketSegmentation import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Clustering.urls'), name='main'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
